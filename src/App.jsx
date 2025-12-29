11c7ae19131c80916d1fbcbbb52dc5deeabca9f6
ankajonly <ankityadav.connect@gmail.com>
2025-12-29 11:39:48 +0530

    1- add app.jsx file

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+import './index.css';
+const App = () => {
+  return (
+    <>
+      <div className="parents">Ankit Yadav</div>
+      <div className="parents">Ankit Yadav</div>
+    </>
+  );
+};
+
+export default App;
